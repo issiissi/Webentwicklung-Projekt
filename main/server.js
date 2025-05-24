32a74b060dcb92f3433292baadc562b1835568ba
Isabell <igr45225@stud.hs-furtwangen.de>
2025-05-24 15:04:16 +0200

    vorlesung test

--- main/server.js
+++ main/server.js
@@ -2,7 +2,6 @@ const http = require('http');
 
 const hostname = '127.0.0.1'; // localhost
 const port = 3000;
-
 const server = http.createServer((request, response) => {
   response.statusCode = 200;
   response.setHeader('Content-Type', 'text/plain');
